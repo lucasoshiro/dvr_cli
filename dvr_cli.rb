109b63f18a8024788dde4ca91c8371becb316728
Lucas Seiki Oshiro <lucasseikioshiro@gmail.com>
2024-08-07 12:35:52 -0300

    feat: simple vlc caller

---
+++ dvr_cli.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+
+RTSP_PORT = 554
+
+Options = Struct.new :host, :user, :password, :channel
+
+class Parser
+  def self.parse(options)
+    args = Options.new("world")
+
+    opt_parser = OptionParser.new do |opts|
+      opts.banner = "Usage: example.rb [options]"
+
+      opts.on "-hHOST", "--host=HOST", "Host" do |host|
+        args.host = host
+      end
+
+      opts.on "-uUSER", "--user=USER", "User" do |user|
+        args.user = user
+      end
+
+      opts.on "-pPASSWORD", "--password=PASSWORD", "Password" do |password|
+        args.password = password
+      end
+
+      opts.on "-cCHANNEL", "--channel=CHANNEL", "Channel" do |channel|
+        args.channel = channel
+      end
+
+      opts.on ""
+    end
+
+    opt_parser.parse!(options)
+    return args
+  end
+end
+
+def open_vlc options
+  rtsp_url = [
+    "rtsp://",
+    "#{options.user}:#{options.password}@#{options.host}:#{RTSP_PORT}",
+    "/cam/realmonitor?",
+    "channel=#{options.channel}&",
+    "subtype=0"
+  ].join
+
+  `vlc "#{rtsp_url}" &> /dev/null`
+end
+
+options = Parser.parse ARGV
+open_vlc options
